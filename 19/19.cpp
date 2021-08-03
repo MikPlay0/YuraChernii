a3d58287a4150f646c9e0a09a4d34efcd2aa4dd9
MikPlay0 <muxaylo37@gmail.com>
2021-08-03 15:10:30 +0300

    Add project files.

---
+++ 19/19.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "UKR");
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Çì³ííà i =" << i << endl;
+    }
+    
+}
+
+
